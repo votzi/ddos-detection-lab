4ff590023b9a863ed85f1a82d30c2684b4e58acd
Juan Jose Mosquera Vivas <juan.mosqueravi@campusucc.edu.co>
2026-03-04 15:31:00 +0000

    Primer commit limpio

--- scripts/influxdb_query_example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -G http://localhost:8086/query \
-     --data-urlencode "db=ryu_monitor" \
-     --data-urlencode "q=SELECT * FROM port_stats" \
-     --header "Accept: application/csv"
